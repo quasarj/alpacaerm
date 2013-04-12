05fd9f1d9551bc8a8c8f5ae3b68f482f3d1ae093
Quasar Jarosz <quasar@ja.rosz.org>
2013-04-12 17:26:00 -0500

    changed calculation to avoid breaking trending

--- erm/scripts/recalc_erm_composite_risk_score.py
+++ erm/scripts/recalc_erm_composite_risk_score.py
@@ -14,6 +14,12 @@ from exception.models import Exception
 if __name__ == "__main__":
     risks = BankRisk.objects.all()
     for risk in risks:
-        # this causes a save currently. The note
-        # on it suggests it may change, so keep an eye on this.
-        risk.update_calc_fields()
+        # this was taken from BankRisk.update_calc_fields().
+        # It's comment suggests it may change, so keep an eye on it!
+        # Note that this does not update the lastCompositeRisk field,
+        # to preserve the "trending" during this recalc.
+
+        risk.inherentRisk = risk.calc_inherent_risk()
+        risk.compositeRisk = risk.calc_composite_risk()
+
+        risk.save()
